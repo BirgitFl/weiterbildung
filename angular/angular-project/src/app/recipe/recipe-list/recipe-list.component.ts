9579ce5b3bea63fd6dea6dadfcd7ff4a4c73ae17
BirgitFl <susi39@online.ms>
2017-06-17 20:14:13 +0200

    Dummy-Rezept

--- angular/angular-project/src/app/recipe/recipe-list/recipe-list.component.ts
+++ angular/angular-project/src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Recipe } from './../recipe.model';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class RecipeListComponent implements OnInit {
-
+  @Output() recipeSelected = new EventEmitter<Recipe>();
+  recipe = new Recipe('Dummy', 'Dummy','http://guides.global/images/guides/global/dummy_web_page.jpg' );
+  selectedRecipe: Recipe;
   constructor() { }
 
   ngOnInit() {
   }
 
+ onSelected(recipe: Recipe){
+   this.recipeSelected.emit(recipe); }
 }
